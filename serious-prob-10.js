58672c9c48beca0f2359440c3180eba890580a55
Hridoy Ahmed <87127289+hridoy8888@users.noreply.github.com>
2023-01-25 22:29:54 +0600

    Delete serious-prob-10.js

--- serious-prob-10.js
+++
@@ -1,9 +0,0 @@
-// 10. Return Something to Me!
-
-function giveMeSomething(vaiInput){
-    return "something " + vaiInput ;
-}
-
-console.log(giveMeSomething("is better than nothing"));
-console.log(giveMeSomething("Bob Jane"));
-console.log(giveMeSomething("something"));
