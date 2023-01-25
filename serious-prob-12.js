444c82aec9d8c0f4c8db3cb9ce0a97dc8ee17760
Hridoy Ahmed <87127289+hridoy8888@users.noreply.github.com>
2023-01-25 22:31:43 +0600

    Delete serious-prob-12.js

--- serious-prob-12.js
+++
@@ -1,9 +0,0 @@
-// 12. Is the Number Less than or Equal to Zero?
-
-function lessThanOrEqualToZero(vaiInput){
-    return (vaiInput <= 0);
-}
-
-console.log(lessThanOrEqualToZero(5));
-console.log(lessThanOrEqualToZero(0));
-console.log(lessThanOrEqualToZero(-2));
