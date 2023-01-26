796d993708a168bbb90d966a64a6a28e71d64438
Hridoy Ahmed <87127289+hridoy8888@users.noreply.github.com>
2023-01-26 08:57:11 +0600

    Delete serious-prob-7.js

--- serious-prob-7.js
+++
@@ -1,9 +0,0 @@
-// 7. Power Calculator 
-
-function circuitPower(volt, curnt){
-    return volt * curnt;
-}
-
-console.log(circuitPower(230, 10));
-console.log(circuitPower(110, 3));
-console.log(circuitPower(480, 20));
