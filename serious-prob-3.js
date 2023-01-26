9c434acb24d8bc214bebc3f41bbd104450545e3f
Hridoy Ahmed <87127289+hridoy8888@users.noreply.github.com>
2023-01-26 08:56:25 +0600

    Delete serious-prob-3.js

--- serious-prob-3.js
+++
@@ -1,9 +0,0 @@
-// 3. Return the Next Number from the Integer Passed
-
-function addition(firstNum){
-    return firstNum + 1;
-}
-
-console.log(addition(0));
-console.log(addition(9));
-console.log(addition(-3));
