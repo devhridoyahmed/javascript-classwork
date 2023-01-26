1080802e07fcca7549bcd994bfe8b8874d213581
Hridoy Ahmed <87127289+hridoy8888@users.noreply.github.com>
2023-01-26 08:56:35 +0600

    Delete serious-prob-4.js

--- serious-prob-4.js
+++
@@ -1,9 +0,0 @@
-// 4. Area of a Triangle
-
-function triArea(firstNum, secondNum){
-    return firstNum * secondNum * .5;
-}
-
-console.log(triArea(3,2));
-console.log(triArea(7,4));
-console.log(triArea(10,10));
